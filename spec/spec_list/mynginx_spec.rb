c1e3b8341c921fcf32168951944692870ca80e96
RyujiKawazoe <kawazoe@val.co.jp>
2015-03-11 17:37:50 +0900

    add nginx test

---
+++ spec/spec_list/mynginx_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe package("nginx") do
+  it { should be_installed }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe service("nginx") do
+  it { should be_running }
+end
